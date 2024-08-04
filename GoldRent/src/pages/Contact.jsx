adc9b4d79205efe7baa8a250fce4b10079a9260e
ZurcieK <jazdanrajabi@gmail.com>
2024-08-04 15:13:47 +0200

    contact form ended

--- GoldRent/src/pages/Contact.jsx
+++ GoldRent/src/pages/Contact.jsx
@@ -20,14 +20,21 @@ const Contact = () => {
   };
 
   return (
-    <div className='relative bg-cover bg-center z-0 h-screen' style={{ backgroundImage: "url(/obrazek1.jpg)" }}>
+    <div className='relative bg-cover bg-center z-0 h-screen' style={{  backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.5)), url(/bg2.jpg)`,
+
+      backgroundSize: "cover",
+
+      backgroundPosition: "center"
+
+
+    }}>
       <div className='relative z-10 xl:container mx-auto mb-32'>
         <div className='flex justify-center' style={{ height: '250px' }}>
-          <h1 className='text-4xl sm:text-4xl md:m-auto md:-mt-1 font-bold font-sans text-black pt-12'>
+          <h1 className='text-4xl sm:text-4xl md:m-auto md:-mt-2 font-bold font-sans text-white pt-12'>
             Skontaktuj się z nami
           </h1>
         </div>
-        <div className='px-4 sm:w-2/3 lg:w-1/2 mx-auto opacity-95'>
+        <div className='px-4 sm:w-2/3 lg:w-1/2 mx-auto '>
           <div className='rounded-lg shadow-lg bg-white -mt-24 py-10 md:py-12 px-4 md:px-6'>
             <div className='grid grid-cols-2 gap-x-6 mb-12 mx-auto'>
               <IconInfo icon={<IconEmail />} text="contact@us" />
@@ -67,8 +74,8 @@ const Contact = () => {
                     <FormElements type="textarea" label="Wiadomość" placeholder="Z czym do nas przychodzisz..." fieldRef={field} hasError={errors.message?.type === 'required'} />
                   )}
                 />
-                <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded mt-4">
-                  Submit
+                <button type="submit" className=" bg-blue-500 text-white py-2 px-4 rounded mt-4 mx-auto block w-full hover:bg-blue-700 ">
+                  Wyślij
                 </button>
               </form>
             </div>
